11334e977096dd92ded6d097852b6bedc5dd60aa
Ian <rutsuki123@gmail.com>
2019-12-06 09:26:05 +0800

    implement interface.

--- MediatorSample/Handlers/QueryUserHandler.cs
+++ MediatorSample/Handlers/QueryUserHandler.cs
@@ -1,16 +1,24 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using MediatR;
 
 namespace MediatorSample.Handlers
 {
     public class QueryUserHandler : IRequestHandler<QueryUserQuery, QueryUserResponse>
     {
+        public Task<QueryUserResponse> Handle(QueryUserQuery request, CancellationToken cancellationToken)
+        {
+        }
     }
 
     public class QueryUserResponse
     {
     }
 
-    public class QueryUserQuery
+    public class QueryUserQuery : IRequest<QueryUserResponse>
     {
+        public int Id { get; set; }
     }
 }
