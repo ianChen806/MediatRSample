75ae9e3424c69ef1244da209fa708fee93e2b1c3
Ian <rutsuki123@gmail.com>
2019-12-06 09:12:34 +0800

    init, add nuget package, add handler.

---
+++ MediatorSample/Handlers/QueryUserHandler.cs
@@ -0,0 +1,16 @@
+using MediatR;
+
+namespace MediatorSample.Handlers
+{
+    public class QueryUserHandler : IRequestHandler<QueryUserQuery, QueryUserResponse>
+    {
+    }
+
+    public class QueryUserResponse
+    {
+    }
+
+    public class QueryUserQuery
+    {
+    }
+}
